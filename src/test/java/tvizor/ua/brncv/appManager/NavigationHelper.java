a9278c2bf77d9d67864115c9c29e8d1ff4a7d12c
Alex <gorshkov.sash@gmail.com>
2019-04-12 16:43:27 +0300

    2 working tests

--- src/test/java/tvizor/ua/brncv/appManager/NavigationHelper.java
+++ src/test/java/tvizor/ua/brncv/appManager/NavigationHelper.java
@@ -3,14 +3,14 @@ package tvizor.ua.brncv.appManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-    private WebDriver driver;
+public class NavigationHelper extends HelperBase{
 
     public NavigationHelper(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     public void gotoGroupPage() {
         driver.findElement(By.linkText("groups")).click();
+
     }
 }
